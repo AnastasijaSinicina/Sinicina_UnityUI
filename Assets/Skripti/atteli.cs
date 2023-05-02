ab445baf7b124081a40c0981e0035280f85cda5a
Anastasija <sinica.nastja@gmail.com>
2023-05-02 20:07:57 +0300

    Pievienoti dropdown, kuri ar toggle palidzibu radas vai slepjas

---
+++ Assets/Skripti/atteli.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atteli : MonoBehaviour {
+
+	public GameObject krekli;
+	public GameObject krekliIzv;
+	public GameObject bikses;
+	public GameObject biksesIzv;
+	public GameObject apavi;
+	public GameObject apaviIzv;
+	public GameObject akses;
+	public GameObject aksesIzv;
+
+
+	public void krekluAttelosana(bool vertiba){
+		krekliIzv.SetActive (vertiba);
+	}
+	public void biksesAttelosana(bool vertiba){
+		biksesIzv.SetActive (vertiba);
+	}
+	public void apaviAttelosana(bool vertiba){
+		apaviIzv.SetActive (vertiba);
+	}
+	public void aksesAttelosana(bool vertiba){
+		aksesIzv.SetActive (vertiba);
+	}
+
+
+}
