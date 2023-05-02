a3b4ee7a18b10079f0dd11e25737ba86ea7b03e5
Anastasija <sinica.nastja@gmail.com>
2023-05-02 07:14:06 +0300

    Pievienotas dažādas ainas un prslēgšana starp tiem

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+	// metode lauj parslegties no ui ainas uz sakumu
+	public void uzSakumu()
+	{
+		SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+	}
+	// metode aizvert programmu
+
+	public void Apturet()
+	{
+		Application.Quit();
+	}
+
+	public void uzIzvele()
+	{
+		SceneManager.LoadScene("Izvele", LoadSceneMode.Single);
+	}
+	public void uzZens(){
+		SceneManager.LoadScene("PR", LoadSceneMode.Single);
+}
+}
